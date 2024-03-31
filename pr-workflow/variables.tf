62a48709d0a1578162c74539432462dcf5c7f785
Dan Houle <dan@azaleahill.net>
2024-03-31 12:48:48 -0700

    remove repo name from pr workflow

--- pr-workflow/variables.tf
+++ pr-workflow/variables.tf
@@ -3,11 +3,6 @@ variable "repository_id" {
   type        = string
 }
 
-variable "repository_name" {
-  description = "The name of the GitHub repository"
-  type        = string
-}
-
 variable "trunk_branch" {
   description = "The name of the trunk branch for the GitHub repository"
   type        = string
