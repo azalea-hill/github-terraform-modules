116c49f2655b05f39c2b29c89b51556b73b6fa47
Dan Houle <dan@azaleahill.net>
2024-03-31 00:14:30 -0700

    update base repo module

--- repository/variables.tf
+++ repository/variables.tf
@@ -1,7 +1,9 @@
 variable "default_branch" {
+  # @todo: Find a way to not require this as a variable.
+  # The issue is that the repo is always created with the default branch set in the organization
+  # and there is no way to read the default branch from the organization.
   description = "The default branch for the organziation or account"
   type        = string
-  default     = "main"
 }
 
 variable "name" {
@@ -18,5 +20,22 @@ variable "description" {
 variable "visibility" {
   description = "The visibility of the repository"
   type        = string
-  default     = "private"
+}
+
+variable "gitignore_template" {
+  description = "The gitignore template to use for the repository"
+  type        = string
+  default     = null
+}
+
+variable "license_template" {
+  description = "The license template to use for the repository"
+  type        = string
+  default     = null
+}
+
+variable "archived" {
+  description = "Archive the repository"
+  type        = bool
+  default     = false
 }
